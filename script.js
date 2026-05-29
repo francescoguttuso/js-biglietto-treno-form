c3ca3c936c2ab7f9cc8e67c40e98b28da9f8f194
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-29 23:41:22 +0200

    feat: enhance ticket calculation logic with age-based discounts

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ const eta = document.getElementById('eta');
 const form = document.querySelector('form');
 const kmInseriti = document.getElementById('km-inseriti');
 const etaInserita = document.getElementById('eta-inserita');
-const prezzoBiglietto = document.getElementById('prezzo-biglietto');
+const prezzoDelBiglietto = document.getElementById('prezzo-biglietto');
 
+
+form.addEventListener('submit', event =>{
+event.preventDefault()
+
+const prezzoPerKm = 0.21;
+const etaUtente = parseInt(eta.value);
+const kmUtente = parseInt(km.value);
+let prezzoBiglietto = kmUtente * prezzoPerKm;
+
+if (etaUtente < 18) {
+    prezzoBiglietto = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
+
+} else if (etaUtente > 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
+
+}
+console.log(prezzoBiglietto);
+
+
+kmInseriti.textContent = `Chilometri dichiarati: ${kmUtente} km`;
+etaInserita.textContent = `Età dichiarata: ${etaUtente} anni`;
+prezzoDelBiglietto.textContent = `Prezzo totale del Biglietto: ${prezzoBiglietto.toFixed(2)}€`;
+});
 
 
  
