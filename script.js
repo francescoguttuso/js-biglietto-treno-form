74d56450c512c27dee610e942b2aa2a413ec8ab3
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-29 23:11:56 +0200

    feat: add initial JavaScript variables for form handling refactor: remove button styles from CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+const chilometri = document.getElementById('km');
+const eta = document.getElementById('eta');
+const form = document.querySelector('form');
+const kmInseriti = document.getElementById('km-inseriti');
+const etaInserita = document.getElementById('eta-inserita');
+const prezzoBiglietto = document.getElementById('prezzo-biglietto');
+
+
+
+ 
