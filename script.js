8d2291d1619f225b5449fa29c4ccef0f27143ef5
francesco-guttuso <francescoguttuso@gmail.com>
2026-06-03 21:51:52 +0200

    refactor: streamline event listener and variable declarations in ticket calculation

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
-const chilometri = document.getElementById('km');
-const eta = document.getElementById('eta');
-const form = document.querySelector('form');
-const kmInseriti = document.getElementById('km-inseriti');
-const etaInserita = document.getElementById('eta-inserita');
-const prezzoDelBiglietto = document.getElementById('prezzo-biglietto');
+const chilometri = document.getElementById("km");
+const eta = document.getElementById("eta");
+const form = document.querySelector("form");
+const kmInseriti = document.getElementById("km-inseriti");
+const etaInserita = document.getElementById("eta-inserita");
+const prezzoDelBiglietto = document.getElementById("prezzo-biglietto");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-form.addEventListener('submit', event =>{
-event.preventDefault()
+    const prezzoPerKm = 0.21;
+    const etaUtente = parseInt(eta.value);
+    const kmUtente = parseInt(km.value);
+    let prezzoBiglietto = kmUtente * prezzoPerKm;
 
-const prezzoPerKm = 0.21;
-const etaUtente = parseInt(eta.value);
-const kmUtente = parseInt(km.value);
-let prezzoBiglietto = kmUtente * prezzoPerKm;
+    if (etaUtente < 18) {
+        prezzoBiglietto = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
+    } else if (etaUtente > 65) {
+        prezzoBiglietto = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
+    }
+    console.log(prezzoBiglietto);
 
-if (etaUtente < 18) {
-    prezzoBiglietto = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
-
-} else if (etaUtente > 65) {
-    prezzoBiglietto = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
-
-}
-console.log(prezzoBiglietto);
-
-
-
-kmInseriti.textContent = `Chilometri dichiarati: ${kmUtente} km`;
-etaInserita.textContent = `Età dichiarata: ${etaUtente} anni`;
-prezzoDelBiglietto.textContent = `Prezzo totale del Biglietto: ${prezzoBiglietto.toFixed(2)}€`;
+    kmInseriti.textContent = `Chilometri dichiarati: ${kmUtente} km`;
+    etaInserita.textContent = `Età dichiarata: ${etaUtente} anni`;
+    prezzoDelBiglietto.textContent = `Prezzo totale del Biglietto: ${prezzoBiglietto.toFixed(2)}€`;
 });
-
-
- 
