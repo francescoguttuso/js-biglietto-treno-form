03ebb84dc236fd69f4330e6c5187ab9b6de9b21b
francesco-guttuso <francescoguttuso@gmail.com>
2026-06-03 21:57:46 +0200

    refactor: improve code readability by standardizing indentation and formatting in ticket calculation

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const etaInserita = document.getElementById("eta-inserita");
 const prezzoDelBiglietto = document.getElementById("prezzo-biglietto");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const prezzoPerKm = 0.21;
-    const etaUtente = parseInt(eta.value);
-    const kmUtente = parseInt(km.value);
-    let prezzoBiglietto = kmUtente * prezzoPerKm;
+  const prezzoPerKm = 0.21;
+  const etaUtente = parseInt(eta.value);
+  const kmUtente = parseInt(km.value);
+  let prezzoBiglietto = kmUtente * prezzoPerKm;
 
-    if (etaUtente < 18) {
-        prezzoBiglietto = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
-    } else if (etaUtente > 65) {
-        prezzoBiglietto = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
-    }
-    console.log(prezzoBiglietto);
+  if (etaUtente < 18) {
+    prezzoBiglietto = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
+  } else if (etaUtente > 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
+  }
+  console.log(prezzoBiglietto);
 
-    kmInseriti.textContent = `Chilometri dichiarati: ${kmUtente} km`;
-    etaInserita.textContent = `Età dichiarata: ${etaUtente} anni`;
-    prezzoDelBiglietto.textContent = `Prezzo totale del Biglietto: ${prezzoBiglietto.toFixed(2)}€`;
+  kmInseriti.textContent = `Chilometri dichiarati: ${kmUtente} km`;
+  etaInserita.textContent = `Età dichiarata: ${etaUtente} anni`;
+  prezzoDelBiglietto.textContent = `Prezzo totale del Biglietto: ${prezzoBiglietto.toFixed(2)}€`;
 });
